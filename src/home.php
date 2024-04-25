7848999139d9d47ef091176a2a2d2a0e68d6e510
Rajeev Kumar <kumarrajeev66797@gmail.com>
2024-04-25 14:38:34 +0530

    changes

--- src/home.php
+++ src/home.php
@@ -1,22 +1,20 @@
-
-        <!-- Header-->
-        
-        <?php include 'partials/header.php' ?>
-        <!-- /#header -->
-        <!-- Content -->
-        <div class="content">
-            <div class="row">
-                <div class="col-lg-9 carousel_col" id="carousel_col">
+<!-- Header-->
+<?php include 'partials/header.php' ?>
+<!-- /#header -->
+<!-- Content -->
+<div class="content">
+    <div class="row">
+        <div class="col-lg-9 carousel_col" id="carousel_col"> 
             <div id="SimpleCarouselExample" class="carousel slide" data-ride="carousel">
                 <div class="carousel-inner" >
                     <div class="carousel-item active">
-                    <img src="../images/img1.jpg" class="d-block w-100"  alt="...">
+                        <img src="../images/img1.jpg" class="d-block w-100"  alt="...">
                     </div>
                     <div class="carousel-item">
-                    <img src="../images/img.jpeg" class="d-block w-100" style="width:300px" alt="...">
+                        <img src="../images/img.jpeg" class="d-block w-100" style="width:300px" alt="...">
                     </div>
                     <div class="carousel-item">
-                    <img src="../images/image2.jpeg" class="d-block w-100" style="width:300px" alt="...">
+                        <img src="../images/image2.jpeg" class="d-block w-100" style="width:300px" alt="...">
                     </div>
                 </div>
                 <a class="carousel-control-prev" href="#SimpleCarouselExample" role="button" data-slide="prev">
@@ -28,13 +26,12 @@
                     <span class="sr-only">Next</span>
                 </a>
             </div>
-</div>
-
-            </div>
         </div>
-        <!-- /.content -->
-        <div class="clearfix"></div>
-        <!-- Footer -->
-        <?php include 'partials/footer.php' ?>
+    </div>
+</div>
+<!-- /.content -->
+<div class="clearfix"></div>
+<!-- Footer -->
+<?php include 'partials/footer.php' ?>
        
 
