d410bcfe0ae168006ee63b9f34d163e075216a1a
Nikunjeds <nikunj@edsglobal.com>
2024-04-25 15:43:06 +0530

    header updated

--- src/home.php
+++ src/home.php
@@ -8,21 +8,13 @@
             <div id="SimpleCarouselExample" class="carousel slide" data-ride="carousel">
                 <div class="carousel-inner" >
                     <div class="carousel-item active">
-<<<<<<< HEAD
                     <img src="../images/i2.png" class="d-block w-100"  alt="...">
-=======
-                        <img src="../images/img1.jpg" class="d-block w-100"  alt="...">
->>>>>>> 5496f87969dcc88a6b30f967d067cb6b5056c555
                     </div>
                     <div class="carousel-item">
                         <img src="../images/img.jpeg" class="d-block w-100" style="width:300px" alt="...">
                     </div>
                     <div class="carousel-item">
-<<<<<<< HEAD
                     <img src="../images/i1.png" class="d-block w-100" style="width:300px" alt="...">
-=======
-                        <img src="../images/image2.jpeg" class="d-block w-100" style="width:300px" alt="...">
->>>>>>> 5496f87969dcc88a6b30f967d067cb6b5056c555
                     </div>
                 </div>
                 <a class="carousel-control-prev" href="#SimpleCarouselExample" role="button" data-slide="prev">
