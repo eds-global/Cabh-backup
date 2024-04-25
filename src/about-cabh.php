0b9719216309fb250d538aed7fcd4341970a5006
Nikunjeds <nikunj@edsglobal.com>
2024-04-25 16:01:08 +0530

    about page updated

--- src/about-cabh.php
+++ src/about-cabh.php
@@ -1,3 +1,18 @@
+<style>
+    .about-content{
+        margin:10px 60px;
+        padding: 20px;
+    }
+    .about-bg{
+        background: white;
+        padding-left:30px;
+    }
+    .heading-about{
+        padding-bottom:20px;
+    }
+    </style>
+
+
 <!-- Header-->
 <?php include 'partials/header.php' ?>
 <!-- /#header -->
@@ -7,39 +22,27 @@
     <div class="animated fadeIn">
        
         <div class="row">
-            <div class="col-lg-1"></div>
-            <div class="col-lg-3 mt-3"> <!-- Added margin-top class -->
-                <div class="col-lg-12">
-                    <p style="color: transparent;">Cleaner Air and Better Health
-                        is a five-year project supported by the United States Agency for International
-                        Development. It aims to strengthen air pollution mitigation and reduce exposure to air pollution in India
-                        by establishing evidence-based models for better air quality management.
-                        The project is being implemented by a consortium led by the Council on Energy, Environment, and Water 
-                        and includes
+            <div class="col-lg-12 text-center">
+                <h4 class="heading-about"> About CABH </h4>
+            </div>
+            <div class="row" >
+                <div class="about-bg">
+                <div class="col-lg-6 about-content" >
+                <p> <!-- Set text alignment to left -->
+                        Cleaner Air and Better Health is a five-year project supported by the United States Agency for International
+                        Development. It aims to strengthen air pollution mitigation and reduce exposure to air pollution in India by establishing evidence-based models for better air quality management.<br>
+                        The project is being implemented by a consortium led by the Council on Energy, Environment, and Water and includes
+                        ASAR Social Impact Advisors, Environmental Design Solutions Defence Firm, and Vital Strategies Legal.
                     </p>
                 </div>
-
-                <div class="talk-bubble tri-right round right-in" style="background-color: #a2a8d3; padding: 15px; border-radius: 10px; color: white; height: 35%; border: 2px solid black;"> <!-- Added border property -->
-                    <p style="color: #000000;">About CABH<br>
-                        Standard text and branding.
-                        High contrast color theme for branding to stand out.
-                    </p>
                 </div>
-
-                <!-- jwedew 
-            <div class="talk-bubble tri-right round right-in" style="margin: 40px; display: inline-block; position: relative; width: 200px; height: auto; background-color: lightyellow;">
-                <div class="talktext" style="padding: 1em; text-align: left; line-height: 1.5em;">
+                <div class="col-lg-6"></div>
+            </div>
+            
                 
-                    About CABH<br>
-                    Standard text and branding.
-                    High contrast color theme for branding to stand out.
-                </div>
-                <div class="tri-right right-in" style="position: absolute; width: 0; height: 0; left: auto; right: -40px; top: 30px; bottom: auto; border: 20px solid; border-color: #666 transparent transparent #666;"></div>
-                <div class="tri-right right-in" style="position: absolute; width: 0; height: 0; left: auto; right: -20px; top: 38px; bottom: auto; border: 12px solid; border-color: lightyellow transparent transparent lightyellow;"></div>
-            </div>
-            -->
 
-            </div>
+                
+
             
             <div class="col-lg-1"></div>
             <div class="col-lg-6 text-center" style="height: 100%;">
